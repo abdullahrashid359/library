d430ff71f18b7843c0830b13bcef12123ba78a7d
Abdullah Rashid <229150035+abdullahrashid359@users.noreply.github.com>
2026-07-15 20:02:15 +0500

    Build library CRUD operations

--- script.js
+++ script.js
@@ -11,8 +11,12 @@ function Book (id, title, author, pages, haveRead) {
     this.haveRead = haveRead;
 }
 
-Book.prototype.info = function() {
+Book.prototype.info = function () {
     return `${this.title} by ${this.author}, ${this.pages}, ${this.haveRead ? "Read" : "Not read yet"}`;
+}
+
+Book.prototype.toggleStatus = function () {
+    this.haveRead = !this.haveRead;
 }
 
 function addBookToLibrary(title, author, pages, haveRead) {
@@ -49,10 +53,10 @@ function displayBooks() {
     for (const book of myLibrary) {
         const card = document.createElement("div");
         card.classList.add("card");
+        card.dataset.id = book.id;
 
         const title = document.createElement("h2");
         title.textContent = book.title;
-        title.classList.add("title");
 
         const author = document.createElement("p");
         author.textContent = "Author: " + book.author;
@@ -62,11 +66,34 @@ function displayBooks() {
 
         const status = document.createElement("p");
         status.textContent = "Status: " + (book.haveRead ? "Read" : "Not read yet");
+        status.classList.add("status")
+
+        const removeBtn = document.createElement("button");
+        removeBtn.textContent = "Remove";
+        removeBtn.addEventListener("click", (event) => {
+            const id = event.target.closest(".card").dataset.id;
+            const index = myLibrary.findIndex(book => book.id === id);
+            myLibrary.splice(index, 1);
+            displayBooks();
+        })
+
+        const toggleBtn = document.createElement("button");
+        toggleBtn.textContent = book.haveRead ? "Mark as unread" : "Mark as read";
+        toggleBtn.addEventListener("click", (event) => {
+            const id = event.target.closest(".card").dataset.id;
+            const index = myLibrary.findIndex(book => book.id === id);
+            myLibrary[index].toggleStatus();
+            event.target.closest(".card").querySelector(".status").textContent = "Status: " + (myLibrary[index].haveRead ? "Read" : "Not read yet");
+            event.target.textContent = myLibrary[index].haveRead ? "Mark as unread" : "Mark as read";
+        })
+        
 
         card.appendChild(title);
         card.appendChild(author);
         card.appendChild(pages);
         card.appendChild(status);
+        card.appendChild(removeBtn);
+        card.appendChild(toggleBtn);
 
         library.appendChild(card);
     }
@@ -74,3 +101,32 @@ function displayBooks() {
 }
 
 displayBooks();
+
+const dialog = document.querySelector("dialog");
+const newBookBtn = document.querySelector("#new-btn");
+const form = document.querySelector("form");
+const closeBtn = document.querySelector("#close-btn");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const selectedRadio = document.querySelector('input[name="status"]:checked');
+
+newBookBtn.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const status = selectedRadio.value === "yes";
+
+    addBookToLibrary(title.value, author.value, pages.value, status);
+    displayBooks();
+    form.reset();
+    dialog.close();
+});
+
+closeBtn.addEventListener("click", () => {
+    form.reset();
+    dialog.close();
+})
+
